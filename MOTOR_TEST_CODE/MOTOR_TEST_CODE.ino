13c5d14f10c1d58cf1ae827aaa61b154d416e5c1
Byron Hawksmith <hawksmithbyron@tutanota.com>
2021-04-16 15:10:34 +0930

    Add additional code to test other L293D

--- MOTOR_TEST_CODE/MOTOR_TEST_CODE.ino
+++ MOTOR_TEST_CODE/MOTOR_TEST_CODE.ino
@@ -2,10 +2,18 @@
 //Motor A
 const int motorPin1 = 12;  // Pin 10 of L293D
 const int motorPin2 = 13;  // Pin 15 of L293D
+
 //Motor B
 const int motorPin3 = 8;   // Pin 2 of L293D
 const int motorPin4 = 11;  // Pin 7 of L293D
 
+//Motor C
+const int motorPin5 = 4;  // Pin 10 of L293D
+const int motorPin6 = 7; // Pin 15 of L293D
+
+//Motor D
+const int motorPin7 = 2;  // Pin 2 of L293D
+const int motorPin8 = 3;  // Pin 7 of L293D
 
 
 //This will run only one time.
@@ -16,6 +24,10 @@ void setup(){
     pinMode(motorPin2, OUTPUT);
     pinMode(motorPin3, OUTPUT);
     pinMode(motorPin4, OUTPUT);
+    pinMode(motorPin5, OUTPUT);
+    pinMode(motorPin6, OUTPUT);
+    pinMode(motorPin7, OUTPUT);
+    pinMode(motorPin8, OUTPUT);
   
 }
 
@@ -49,11 +61,45 @@ void loop(){
     digitalWrite(motorPin3, LOW);
     digitalWrite(motorPin4, HIGH);
     delay(2000);    
-    
+
     //And this code will stop motors
     digitalWrite(motorPin1, LOW);
     digitalWrite(motorPin2, LOW);
     digitalWrite(motorPin3, LOW);
     digitalWrite(motorPin4, LOW);
+
+    //This code  will turn Motor C clockwise for 2 sec.
+    digitalWrite(motorPin5, HIGH);
+    digitalWrite(motorPin6, LOW);
+    digitalWrite(motorPin7, LOW);
+    digitalWrite(motorPin8, LOW);
+    delay(2000); 
+    
+    //This code will turn Motor C counter-clockwise for 2 sec.
+    digitalWrite(motorPin5, LOW);
+    digitalWrite(motorPin6, HIGH);
+    digitalWrite(motorPin7, LOW);
+    digitalWrite(motorPin8, LOW);
+    delay(2000);
+    
+    //This code will turn Motor D clockwise for 2 sec.
+    digitalWrite(motorPin5, LOW);
+    digitalWrite(motorPin6, LOW);
+    digitalWrite(motorPin7, HIGH);
+    digitalWrite(motorPin8, LOW);
+    delay(2000); 
+    
+    //This code will turn Motor D counter-clockwise for 2 sec.
+    digitalWrite(motorPin5, LOW);
+    digitalWrite(motorPin6, LOW);
+    digitalWrite(motorPin7, LOW);
+    digitalWrite(motorPin8, HIGH);
+    delay(2000); 
+
+    //And this code will stop motors
+    digitalWrite(motorPin5, LOW);
+    digitalWrite(motorPin6, LOW);
+    digitalWrite(motorPin7, LOW);
+    digitalWrite(motorPin8, LOW);
 
 }

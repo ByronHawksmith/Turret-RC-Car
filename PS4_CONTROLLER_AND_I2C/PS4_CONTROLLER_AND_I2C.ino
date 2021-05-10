1755cb929b9f8e6c37d678b1837c419fdde5c9df
Byron Hawksmith <hawksmithbyron@tutanota.com>
2021-05-10 16:52:47 +0930

    Add in Wire I2C code.

--- PS4_CONTROLLER_AND_I2C/PS4_CONTROLLER_AND_I2C.ino
+++ PS4_CONTROLLER_AND_I2C/PS4_CONTROLLER_AND_I2C.ino
@@ -12,13 +12,33 @@ void setup() {
     Serial.print(F("\r\nOSC did not start"));
     while (1); // Halt
   }
+  
   Serial.print(F("\r\nPS4 Bluetooth Library Started"));
+
+  Wire.begin();
 }
 
 void loop() {
   Usb.Task();
 
-  if (PS4.connected()) {
-    Serial.print(F("\r\nHello World"));
+  if (PS4.connected()) {    
+    int left_hat_x = PS4.getAnalogHat(LeftHatX);
+    int left_hat_y = PS4.getAnalogHat(LeftHatY);
+
+    byte left_hat_x_lsb = left_hat_x & 0xff;
+    byte left_hat_x_msb = left_hat_x >> 8;
+    byte left_hat_y_lsb = left_hat_y & 0xff;
+    byte left_hat_y_msb = left_hat_y >> 8;
+
+    byte left_hat_bytes[] = {
+      left_hat_x_lsb,
+      left_hat_x_msb,
+      left_hat_y_lsb,
+      left_hat_y_msb
+    };
+    
+    Wire.beginTransmission(8);                            // transmit to device #8
+    Wire.write(left_hat_bytes, sizeof(left_hat_bytes));   // send bytes for left hat
+    Wire.endTransmission();                               // stop transmitting
   }
 }
